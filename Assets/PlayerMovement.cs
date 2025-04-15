62df0f27002e2e1553e6a538c2baee1f73bb9037
3L1X1 <basitq0117@gmail.com>
2025-04-15 12:02:30 -0400

    455634rt
    fdgghgf

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
 
-    // Update is called once per frame
+    Vector2 movement;
+
     void Update()
     {
-        
+        // Collect user input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // Update Animator parameter
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        // Move player using physics
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
