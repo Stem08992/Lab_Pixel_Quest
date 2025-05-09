b422c0524b27e8e7fa9d7045cd70b75c5c0a1c66
nothyl <hanyuli488@gmail.com>
2025-05-09 02:13:19 -0400

    ijhyftrfdguio

--- Assets/Final_Project/Tutorial.cs
+++ Assets/Final_Project/Tutorial.cs
@@ -9,6 +9,11 @@ public class Tutorial : MonoBehaviour
     public List<string> dialogue = new List<string>();
     private TextMeshProUGUI _talkText;
     private int _talkIndex = 0;
+    private RectTransform rt;
+
+
+    //Tranfrom or
+    //Tranfom new
 
     // Start is called before the first frame update
     void Start()
@@ -31,6 +36,14 @@ public class Tutorial : MonoBehaviour
             }
             else
             {
+                if (_talkIndex == 0)
+                {
+                    transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+                    GetComponent<RectTransform>().anchoredPosition = new Vector2(-100, 200);
+
+                }
+                //If index == 4
+                // tansfomr.postion = new.postion;
                 _talkIndex++;
                 _talkText.text = dialogue[_talkIndex];
             }
