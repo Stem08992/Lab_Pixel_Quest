e308b91d46f7008c4efdbfdde66c0d83d943ddf6
Rsbasket <rsbasket3@gmail.com>
2025-07-21 12:01:08 -0400

    jumping physics

--- Assets/Pixel_Quest/Scripts/Player Jump.cs
+++ Assets/Pixel_Quest/Scripts/Player Jump.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class PlayerJump : MonoBehaviour
 {
-    // Start is called before the first frame update
+   public float CapsuleHeight = 0.25f;
+    public float CapsuleRadius = 0.08f;
+   
+    public Transform feetCollider;
+    public LayerMask groundMask;
+    private bool _groundCheck;
+
+    public float jumpForce = 5f;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;
+    private float fallForce = -1;
+    private Vector2 gravityForce;
+
     void Start()
     {
-        
+       gravityForce = new Vector2(0f, Physics2D.gravity.y);
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _groundCheck = Physics2D.OverlapCapsule(
+            point: feetCollider.position,
+            size: new Vector2(CapsuleHeight, CapsuleRadius), CapsuleDirection2D.Horizontal,
+            angle: 0, groundMask);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
+        }
     }
-}
+
+    // Simple ground check using collision
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}   
