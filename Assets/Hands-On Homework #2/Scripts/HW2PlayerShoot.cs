25ce377081f05c57463f5b1830bfb8a8ec316542
Rsbasket <rsbasket3@gmail.com>
2025-07-20 23:28:49 -0400

    update

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject preFab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(preFab, bulletSpawn.position, bulletSpawn.rotation);
+            bullet.transform.SetParent(bulletTrash);
+        }
+    }
 }
