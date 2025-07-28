43e3765d04cc638baf90f0b18c45652e9ad711c2
Rsbasket <rsbasket3@gmail.com>
2025-07-28 12:15:21 -0400

    UPdates
    coin and heart UI

--- Assets/Pixel_Quest/Scripts/Player Stats.cs
+++ Assets/Pixel_Quest/Scripts/Player Stats.cs
@@ -5,19 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
-    //public string nextLevel = "GeoLevel_2";
     public Transform respawnPoint;
     private int coinCounter = 0;
     public int _health = 3;
     public int maxHealth = 3;
     private PlayerUIController _playerUIController;
+    public int CoinInLevel = 0;
 
     private void Start()
     {
         _playerUIController = GetComponent<PlayerUIController>();
+        _playerUIController.StartUI();
         _playerUIController.UpdateHealth(_health, maxHealth);
+        CoinInLevel = GameObject.Find("Coins").transform.childCount;
+        _playerUIController.UpdateText(coinCounter + "/" + CoinInLevel);
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,7 +27,15 @@ public class PlayerStats : MonoBehaviour
         {
             case "Respawn":
                 {
-                    respawnPoint.position = other.transform.Find("Point").position;
+                    Transform point = other.transform.Find("Point");
+                    if (point != null)
+                    {
+                        respawnPoint.position = point.position;
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Respawn trigger is missing a child named 'Point'.");
+                    }
                     break;
                 }
             case "Death":
@@ -34,38 +44,44 @@ public class PlayerStats : MonoBehaviour
                     _playerUIController.UpdateHealth(_health, maxHealth);
                     if (_health <= 0)
                     {
-                        string thislevel= SceneManager.GetActiveScene().name;
+                        string thislevel = SceneManager.GetActiveScene().name;
                         SceneManager.LoadScene(thislevel);
                     }
                     else
                     {
                         transform.position = respawnPoint.position;
                     }
-    
                     break;
                 }
             case "Coin":
                 {
                     coinCounter++;
+                    _playerUIController.UpdateText(coinCounter + "/" + CoinInLevel);
                     Destroy(other.gameObject);
                     break;
                 }
             case "Finish":
                 {
-                  string nextLevel = other.GetComponent<LevelGoals>().nextLevel;
-                    SceneManager.LoadScene(nextLevel);
+                    LevelGoals goals = other.GetComponent<LevelGoals>();
+                    if (goals != null)
+                    {
+                        string nextLevel = goals.nextLevel;
+                        SceneManager.LoadScene(nextLevel);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Finish trigger is missing the LevelGoals component.");
+                    }
                     break;
                 }
             case "Health":
                 {
-                  if (_health < 3)
+                    if (_health < 3)
                     {
                         _health++;
                         _playerUIController.UpdateHealth(_health, maxHealth);
                         Destroy(other.gameObject);
-                   
                     }
-                 
                     break;
                 }
         }
