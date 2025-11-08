b241cf108f836daf9ae0ae8d9408262cd15689cf
ManuelDLC10 <manueldlc.z10@gmail.com>
2025-11-08 12:20:32 -0500

    work8

--- Assets/Pixel_Quest/Scripts/Player Stats.cs
+++ Assets/Pixel_Quest/Scripts/Player Stats.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
+    public Transform respawnPoint;
     public string nextLevel = "GeoLevel_2";
+    private int coinCounter = 0;
+    private int _health = 3;
+    private int _maxHealth = 3;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,13 +19,49 @@ public class PlayerStats : MonoBehaviour
         {
             case "Finish":
                 {
+                    string nextLevel = collision.transform.GetComponent<LevelGoal>().nextLevel;
                     SceneManager.LoadScene(nextLevel);
                     break;
                 }
-            case "Death":
+
+            case "Health":
                 {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
+                   if (_health < _maxHealth)
+                    {
+                        Destroy(collision.gameObject);
+                        _health++;
+                    }
+
+                    break;
+                }
+
+            case "Coin":
+                {
+                    coinCounter++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+
+            case "Death":
+               
+              
+                {
+                   _health--;
+                    if (_health <= 0)
+                    {
+                        string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thisLevel);
+                        Debug.Log("Death");
+                    }
+                    else
+                    {
+                        transform.position = respawnPoint.position;
+                    }
+                    break;
+                }
+            case "Respawn":
+                {
+                    respawnPoint.position = collision.transform.Find("Point").position;
                     break;
                 }
         }
